934892a39de76f911f30f07416153eb510bd0ac9
Shubham Bharde Work <shubhambhardework@gmail.com>
2026-06-07 22:08:33 +0530

    added Employee domain model/entity

---
+++ Employee/Employee.API/Models/Domain/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace Employee.API.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public required string City { get; set; }
+        public required string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
